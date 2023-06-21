9a622e28bdc72cc9cd1298850c7c3c678299cdf4
SevioCorrea <102820124+SevioCorrea@users.noreply.github.com>
2023-06-21 11:47:17 -0400

    CoreData

---
+++ TaskManager/Utility/Constant.swift
@@ -0,0 +1,21 @@
+//
+//  Constant.swift
+//  TaskManager
+//
+//  Created by Sévio Basilio Corrêa on 21/06/23.
+//
+
+import SwiftUI
+
+// MARK: - Formatter
+
+let itemFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .short
+    formatter.timeStyle = .medium
+    return formatter
+}()
+
+// MARK: - UI
+
+// MARK: - UX
