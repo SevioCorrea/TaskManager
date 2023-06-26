699cb8be3d3f287bb60ec00f63ac476beacbda5e
SevioCorrea <102820124+SevioCorrea@users.noreply.github.com>
2023-06-26 13:15:31 -0400

    ListRowItemView

---
+++ TaskManager/View/ListRowItemView.swift
@@ -0,0 +1,25 @@
+//
+//  ListRowItemView.swift
+//  TaskManager
+//
+//  Created by Sévio Basilio Corrêa on 26/06/23.
+//
+
+import SwiftUI
+
+struct ListRowItemView: View {
+    // MARK: - Properties
+
+    // MARK: - Body
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+// MARK: - Preview
+
+struct ListRowItemView_Previews: PreviewProvider {
+    static var previews: some View {
+        ListRowItemView()
+    }
+}
